3f942f54f94e5647f694d7a5983a99daf5dd0b18
eunjeong0417 <82692118+eunjeong0417@users.noreply.github.com>
2021-08-30 13:07:42 +0900

    server connection

---
+++ server/app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.urlencoded({limit:"30mb", extended: true}));
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(cors());
+
+
+//몽고디비연결과 PORT 설정
+const mongodbURL = 'mongodb+srv://jeong:jeong1234567@cluster0.urjey.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+//데이터베이스에 연결
+//mongoose연결시 발생하는 오류 막기위한 설정
+mongoose
+    .connect(mongodbURL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`server running on port ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+
